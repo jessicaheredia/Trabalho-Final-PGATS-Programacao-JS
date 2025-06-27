018bb2cf9a974c95ada6b4a01c8eaa3c71215b34
Jessica Heredia <jessica.heredia@outlook.com>
2025-06-27 17:41:40 -0300

    ci: adicionando playwright config

---
+++ playwright.config.js
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from '@playwright/test';
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  
+  reporter: [
+    ['html', { open: false }],
+    ['junit', { outputFile: `results.xml` }],
+  ],
+  use: {
+    baseURL: 'https://pgats-ci-example.netlify.app',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
